ef16387f231620f6832692035e4e0b7debc48b62
Joe Anderson <40678217+theKnary@users.noreply.github.com>
2024-11-28 13:35:07 -0500

    change transaction class to follow singleton design pattern
    change Transaction class to singleton
    change LibraryManagement to follow Transaction singleton implementation

--- Transaction.java
+++ Transaction.java
@@ -2,36 +2,51 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Transaction {
+	private static Transaction instance;
 
-    // Perform the borrowing of a book
-    public static boolean borrowBook(Book book, Member member) {
-        if (book.isAvailable()) {
-            book.borrowBook();
-            member.borrowBook(book); 
-            String transactionDetails = getCurrentDateTime() + " - Borrowing: " + member.getName() + " borrowed " + book.getTitle();
-            System.out.println(transactionDetails);
-            return true;
-        } else {
-            System.out.println("The book is not available.");
-            return false;
-        }
-    }
+	// Singleton of transaction instance
+	public Transaction getTransaction() {
+		if (instance == null)
+			instance = new Transaction();
 
-    // Perform the returning of a book
-    public static void returnBook(Book book, Member member) {
-        if (member.getBorrowedBooks().contains(book)) {
-            member.returnBook(book);
-            book.returnBook();
-            String transactionDetails = getCurrentDateTime() + " - Returning: " + member.getName() + " returned " + book.getTitle();
-            System.out.println(transactionDetails);
-        } else {
-            System.out.println("This book was not borrowed by the member.");
-        }
-    }
+		return instance;
+	}
 
-    // Get the current date and time in a readable format
-    private static String getCurrentDateTime() {
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        return sdf.format(new Date());
-    }
+	private Transaction() {
+
+	}
+
+	// Perform the borrowing of a book
+	public boolean borrowBook(Book book, Member member) {
+		if (book.isAvailable()) {
+			book.borrowBook();
+			member.borrowBook(book);
+			String transactionDetails = getCurrentDateTime() + " - Borrowing: " + member.getName() + " borrowed "
+					+ book.getTitle();
+			System.out.println(transactionDetails);
+			return true;
+		} else {
+			System.out.println("The book is not available.");
+			return false;
+		}
+	}
+
+	// Perform the returning of a book
+	public void returnBook(Book book, Member member) {
+		if (member.getBorrowedBooks().contains(book)) {
+			member.returnBook(book);
+			book.returnBook();
+			String transactionDetails = getCurrentDateTime() + " - Returning: " + member.getName() + " returned "
+					+ book.getTitle();
+			System.out.println(transactionDetails);
+		} else {
+			System.out.println("This book was not borrowed by the member.");
+		}
+	}
+
+	// Get the current date and time in a readable format
+	private String getCurrentDateTime() {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		return sdf.format(new Date());
+	}
 }
