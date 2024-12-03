e386f82aee0491ab783450b71dcf6b93978973e0
Joe Anderson <40678217+theKnary@users.noreply.github.com>
2024-12-03 16:18:10 -0500

    Add borrow and return book test cases

--- LibraryManagementTest.java
+++ LibraryManagementTest.java
@@ -1,12 +1,15 @@
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 class LibraryManagementTest {
 	@Nested
-	class BookIdTest {
+	static class BookIdTest {
 		@Test
 		void testBoudaryBookIds() {
 
@@ -49,4 +52,69 @@ class LibraryManagementTest {
 		}
 
 	}
+
+	@Nested
+	static class BorrowReturnTest {
+		static Book book;
+		static Member member;
+		static Transaction transaction;
+
+		@BeforeAll
+		static void Init() {
+			// create book
+			try {
+				book = new Book(100, "book");
+				assertEquals(book.getTitle(), "book");
+			} catch (Exception e) {
+				e.printStackTrace();
+				assertNull(e);
+			}
+
+			// create member
+			member = new Member(0, "member");
+			assertEquals(member.getName(), "member");
+
+			// create transaction
+			transaction = Transaction.getTransaction();
+		}
+
+		@Test
+		void testBookAvail() {
+			assertEquals(book.getTitle(), "book");
+		}
+
+		@Test
+		void testMemberExists() {
+			assertEquals(member.getName(), "member");
+		}
+
+		@Test
+		void testBorrow() {
+			boolean didBorrowBook = transaction.borrowBook(book, member);
+			assertTrue(didBorrowBook);
+			assertFalse(book.isAvailable());
+		}
+
+		@Test
+		void testBorrowFail() {
+			boolean didBorrowBook = transaction.borrowBook(book, member);
+			assertFalse(didBorrowBook);
+			assertFalse(book.isAvailable());
+		}
+
+		@Test
+		void testReturn() {
+			boolean didReturnbook = transaction.returnBook(book, member);
+			assertTrue(didReturnbook);
+			assertTrue(book.isAvailable());
+		}
+
+		@Test
+		void testReturnFail() {
+			boolean didBorrowBook = transaction.borrowBook(book, member);
+			assertFalse(didBorrowBook);
+			assertTrue(book.isAvailable());
+		}
+	}
+
 }
