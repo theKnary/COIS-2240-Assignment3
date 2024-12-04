c1229fe219700e23a158cbfd4a91974b033eb344
Joe Anderson <40678217+theKnary@users.noreply.github.com>
2024-12-03 20:12:56 -0500

    add comments

--- LibraryManagementTest.java
+++ LibraryManagementTest.java
@@ -18,24 +18,30 @@ class LibraryManagementTest {
 	static class BookIdTest {
 		@Test
 		void testBoudaryBookIds() {
-
 			try {
+				// Book should be valid
 				Book book = new Book(100, "book");
 				assertEquals(book.getTitle(), "book");
 			} catch (Exception e) {
-				assertNull(e);
-			}
-			try {
-				Book book = new Book(999, "book");
-				assertEquals(book.getTitle(), "book");
-			} catch (Exception e) {
+				// should be no error
 				assertNull(e);
 			}
 
 			try {
+				// Book should be valid
+				Book book = new Book(999, "book");
+				assertEquals(book.getTitle(), "book");
+			} catch (Exception e) {
+				// should be no error
+				assertNull(e);
+			}
+
+			try {
+				// Book should be invalid
 				Book book = new Book(1000, "book");
 				assertNull(book);
 			} catch (Exception e) {
+				// Error should show
 				assertEquals(e.getMessage(), "ID must be 100-999");
 			}
 		}
@@ -44,15 +50,19 @@ class LibraryManagementTest {
 		void testInvalidBookId() {
 
 			try {
+				// Book should be invalid
 				Book book = new Book(0, "book");
 				assertNull(book);
 			} catch (Exception e) {
+				// Error should show
 				assertEquals(e.getMessage(), "ID must be 100-999");
 			}
 			try {
+				// Book should be invalid
 				Book book = new Book(5000, "book");
 				assertNull(book);
 			} catch (Exception e) {
+				// Error should show
 				assertEquals(e.getMessage(), "ID must be 100-999");
 			}
 		}
@@ -70,14 +80,14 @@ class LibraryManagementTest {
 		static void Init() {
 			// create book
 			try {
+				// should be valid
 				book = new Book(100, "book");
 				assertEquals(book.getTitle(), "book");
 			} catch (Exception e) {
-				e.printStackTrace();
 				assertNull(e);
 			}
 
-			// create member
+			// create member (valid)
 			member = new Member(0, "member");
 			assertEquals(member.getName(), "member");
 
@@ -87,45 +97,55 @@ class LibraryManagementTest {
 
 		@Test
 		@Order(1)
+		// check book is valid
 		void testBookAvail() {
 			assertEquals(book.getTitle(), "book");
 		}
 
 		@Test
 		@Order(2)
+		// check member is valid
 		void testMemberExists() {
 			assertEquals(member.getName(), "member");
 		}
 
 		@Test
 		@Order(3)
+		// book should borrow
 		void testBorrow() {
 			boolean didBorrowBook = transaction.borrowBook(book, member);
 			assertTrue(didBorrowBook);
+			// book should no longer be available
 			assertFalse(book.isAvailable());
 		}
 
 		@Test
 		@Order(4)
+		// book should not borrow
 		void testBorrowFail() {
 			boolean didBorrowBook = transaction.borrowBook(book, member);
 			assertFalse(didBorrowBook);
+			// book should not be available
 			assertFalse(book.isAvailable());
 		}
 
 		@Test
 		@Order(5)
+		// book should return
 		void testReturn() {
 			boolean didReturnbook = transaction.returnBook(book, member);
 			assertTrue(didReturnbook);
+			// book should now be available
 			assertTrue(book.isAvailable());
 		}
 
 		@Test
 		@Order(6)
+		// book should not return
 		void testReturnFail() {
 			boolean didReturnBook = transaction.returnBook(book, member);
 			assertFalse(didReturnBook);
+			// book should still be available
 			assertTrue(book.isAvailable());
 		}
 	}
@@ -135,16 +155,20 @@ class LibraryManagementTest {
 		@Test
 		void testSingletonTransaction() {
 			try {
+				// constructor should be private
 				Constructor<Transaction> constructor = Transaction.class.getDeclaredConstructor();
 				assertEquals(constructor.getModifiers(), Modifier.PRIVATE);
 			} catch (NoSuchMethodException | SecurityException e) {
+				// should be no error
 				assertNull(e);
 			}
 
 		}
 
+		// BONUS
 		@Test
 		void testSingletonTransactionGet() {
+			// transaction instances should be the same (prove singleton)
 			Transaction transaction = Transaction.getTransaction();
 			Transaction transaction2 = Transaction.getTransaction();
 			assertEquals(transaction, transaction2);
